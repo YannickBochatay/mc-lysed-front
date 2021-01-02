f78858d4f8e1d6fe076339510439e8d469434c46
1testsample <frankomarmier@gmail.com>
2021-01-03 00:32:03 +0100

    Remove unused files

--- src/components/simulateur/SimulatorSlider.jsx
+++
@@ -1,9 +0,0 @@
-import Slider from "@material-ui/core/Slider";
-import { withStyles } from "@material-ui/core/styles";
-
-const SimulatorSlider = (values) => (props) => {
-
-  return 
-};
-
-export default SimulatorSlider;
